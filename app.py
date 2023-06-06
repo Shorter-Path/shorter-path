80ea87e16f3e035dbc630c9ae82735ce12fbd529
unknown <bishal.upadhyaya@gmail.com>
2023-06-05 20:14:31 -0700

    updated prompt. testing is limited. output is good but inconsistent

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import openai
 import streamlit as st
 
 
+openai.api_key  = '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'
+user = 'Bishal'
+background = "Data Science, Deep Learning, Computer Vision, Neural Architecture Search, Pruning, Algorithmic Finance Trading"
+
+
 def show_messages(text):
     messages_str = [
         f"{_['role']}: {_['content']}" for _ in st.session_state["messages"][1:]
@@ -9,18 +14,21 @@ def show_messages(text):
     text.text_area("Messages", value=str("\n".join(messages_str)), height=400)
 
 
-openai.api_key  = 'your openapi key'
 
-BASE_PROMPT = [ {'role':'system', 'content':"""
-You are Explainer, a service to simplify complex topics for newer learners. \\
-Once the user provides their background, you ask for a 'complex paragraph' that the user wants explained. This paragraph is not necessarily related to their background. \
-Identify terms from the 'complex paragraph' that the user may not be aware of, according to their background. Do not include terms that the user will be familar with.
-First, output the terms and define them in simplified language, in a table format.
-Finally, rewrite the provided paragraph in a simplified manner,  while still using the terms defined in the table. 
+
+BASE_PROMPT = [ {'role':'system', 'content':f"""
+Output only the following:
+1. Complex terms and every abbreviations within the string (ignore common sense terms or abbreviations such as state-of-the-art (SOTA))
+2. A table of terms and abbreviations, with definitions and a score of user knowledge level. Columns are as follows:
+- term or abbreviation
+- long form of abbreviation (for terms, write NA)
+- definition of term or abbreviation in context of the paragraph
+- Assuming the user claims to have a background in {background}, provide a score from 0-100 that predicts how likely they are to know each term
+3. The 'complex string' is rewritten with the same sentence structure that the user provided. Pay extra attention to and explain every term with a score of 6 or below.
+
 """},
 
-{'role':'assistant', 'content':"""
-Welcome to ShorterPath. What is your background (A list of skills and areas of interest, comma delimited)"""} 
+{'role':'assistant', 'content': f""" Welcome to ShorterPath, {user}. What string would you like to simplify?"""} 
  ]
 
 
